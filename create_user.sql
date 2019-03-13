4d2b3c3b1382a8bd7adc86b54d20bbd6de87713c
TangyTortoise <tangytortoise@outlook.com>
2019-03-13 12:51:42 +0000

    Removed test files and added create_user.sql

---
+++ create_user.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `s4922402`.`user` (
+  `usr_id` INT NOT NULL AUTO_INCREMENT,
+  `usr_email` VARCHAR(100) NOT NULL,
+  `usr_my_key` VARCHAR(45) NOT NULL,
+  `usr_first_name` VARCHAR(45) NOT NULL,
+  `usr_last_name` VARCHAR(45) NOT NULL,
+  `usr_bio` TEXT(1000) NULL,
+  `usr_assigned_lecturer` INT NULL,
+  `usr_auth_level` INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`usr_id`),
+  UNIQUE INDEX `id_UNIQUE` (`usr_id` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`usr_email` ASC));
